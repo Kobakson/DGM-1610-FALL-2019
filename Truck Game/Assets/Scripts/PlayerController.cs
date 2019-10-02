dc1a3dd76ee9ba382952716fa906eb6b9d3a8e3a
Kobakson <kobaksonmods@gmail.com>
2019-10-03 00:16:15 +0200

    Truck Game Script Update
    Added controls functionality, forward and turning input.

--- Truck Game/Assets/Scripts/PlayerController.cs
+++ Truck Game/Assets/Scripts/PlayerController.cs
@@ -6,19 +6,22 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     public float turnSpeed;
-    // Start is called before the first frame update
+    public float horizontalInput;
+    public float forwardInput;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         // This code moves the vehicle forward.
         // transform.Translate(0, 0, 1);
         //transform.Translate(Vector3.forward * Time.deltaTime * 5);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
