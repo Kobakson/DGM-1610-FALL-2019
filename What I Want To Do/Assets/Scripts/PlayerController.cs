3c9d9ec9c3108a981901802dbbc3d5e467ee9f3d
Kobakson <kobaksonmods@gmail.com>
2019-12-05 01:36:57 +0100

    Movement Script
    For personal project: destruction/combat game

---
+++ What I Want To Do/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        // playerRb.AddForce(Vector3.forward * forwardInput * speed);
+        // playerRb.AddForce(Vector3.right * horizontalInput * speed);
+        playerRb.velocity = (transform.forward * forwardInput * speed * Time.deltaTime);
+        playerRb.velocity = (transform.right * horizontalInput * speed * Time.deltaTime);
+    }
+
+
+}
